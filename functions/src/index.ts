c2394fa4bf8c51bf66dc39fd1d295279891c8bd8
Ivan Voevodin <voevodin36@gmail.com>
2019-12-21 16:30:30 +0400

    Change routing to express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,44 @@
-import { https } from "firebase-functions";
+import { region } from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from "express";
 
 type DocumentData = admin.firestore.DocumentData;
 
+const app = express();
 admin.initializeApp();
 
-export const getComments = https.onRequest((request, response) => {
-    admin.firestore().collection("comments").get()
+app.get("/comments", (request, response) => {
+    admin.firestore()
+        .collection("comments")
+        .orderBy("commentTime", "desc")
+        .get()
         .then(data => {
             const comments: DocumentData[] = [];
-            data.forEach(document => comments.push(document.data()));
+            data.forEach(document => comments.push({
+                commentId: document.id,
+                userName: document.data().userName,
+                comment: document.data().comment,
+                commentTime: document.data().commentTime
+            }));
             return response.json(comments);
         })
         .catch(error => console.error(error))
 });
 
-export const createComment = https.onRequest((request, response) => {
-    if (request.method !== "POST") {
-        response.status(400).json({error: "Method not allowed"});
-    } else {
-        const comment = {
-            userName: request.body.userName,
-            comment: request.body.comment,
-            commentTime: admin.firestore.Timestamp.fromDate(new Date())
-        };
+app.post("/comments", (request, response) => {
+    const comment = {
+        userName: request.body.userName,
+        comment: request.body.comment,
+        commentTime: new Date().toISOString()
+    };
 
-        admin.firestore().collection("comments").add(comment)
-            .then(document => response.json({message: `Document ${document.id} created successfully`}))
-            .catch(error => {
-                response.status(500).json({error: "Error when try to create comment"});
-                console.error(error);
-            })
-    }
+    admin.firestore().collection("comments").add(comment)
+        .then(document => response.json({message: `Document ${document.id} created successfully`}))
+        .catch(error => {
+            response.status(500).json({error: "Error when try to create comment"});
+            console.error(error);
+        })
 });
+
+// eslint-disable-next-line import/prefer-default-export
+export const api = region("europe-west1").https.onRequest(app);
